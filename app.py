53540122bbfb910ebcaf8dfc9a6a8e6872b84b87
Kanha Nahak <kanhanahak17@gmail.com>
2024-10-24 13:18:08 +0530

    latest updated

---
+++ app.py
@@ -0,0 +1,16 @@
+import joblib
+import numpy as np
+import streamlit as st
+st.title("Sales predictor")
+st.image("naresh_it.jpeg")
+
+TV=st.number_input("please enter sales of Tv")
+Newspaper=st.number_input("please enter sales of Newspaper")
+Radio=st.number_input("please enter sales of Radio")
+
+model=joblib.load("model.pkl")
+
+if st.button("prediction"):
+    features=np.array([[TV,Newspaper,Radio]])
+    output=model.predict(features)
+    st.write(f"the total sales is {output[0]}")
